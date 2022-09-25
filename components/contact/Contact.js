5970151a1a9bd471786ee61b6e271fd07bbf87c5
cowsoncows <coffedrop@live.com>
2022-09-24 22:06:29 -0400

    Refresh 6

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -2,7 +2,7 @@ import classes from '../contentPage.module.css'
 import Accordion from '../accordion/Accordion.js'
 import Slidein from '../slidein/Slidein.js'
 import Link from '../link/Link.js'
-// Comment 5
+// Comment 6
 
 function Contact() {
     return (
