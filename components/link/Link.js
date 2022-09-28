7609e30ccec0a500c562aecacb14a88446284d05
BROD NICKY <coffedrop@live.com>
2022-09-28 13:28:58 -0400

    Link colour fix

--- components/link/Link.js
+++ components/link/Link.js
@@ -5,7 +5,7 @@ import classes from './link.module.css'
 function LinkComponent({ href, children, style, color }) {
     return (
         <motion.span 
-            initial={{  textDecorationThickness: '0.1em', color: color || 'inherit' }}
+            initial={{  textDecorationThickness: '0.1em', color: color }}
             whileHover={{ color: '#EC994B', textDecorationThickness: '0.25em' }}
             className={classes.link} 
             style={style}
