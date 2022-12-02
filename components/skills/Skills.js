b538307ccdb4e9aad7dc8dcce4435e87cd5db52e
BROD NICKY <coffedrop@live.com>
2022-12-02 17:38:01 -0500

    Skill Test

--- components/skills/Skills.js
+++ components/skills/Skills.js
@@ -2,16 +2,19 @@ import { motion } from 'framer-motion'
 import Skill from './skill/Skill.js'
 import Slidein from '../slidein/Slidein.js'
 import classes from '../contentPage.module.css'
+import skillStyle from './skill/skill.module.css'
 
 function Skills(props) {
     return(
         <div className={classes.contentPage} id="skills">
-            <Slidein>
-                <h1 style={{textAlign: 'left'}}>Skills</h1>
+            <div className={classes.contents}>
+                <Slidein>
+                    <h1 style={{textAlign: 'left'}}>Skills</h1>
+                </Slidein>
                 <div>
-                    <p1>Test</p1>
+                    <p className={skillStyle.skill}>Test</p>
                 </div>
-            </Slidein>
+            </div>
         </div>
     ) 
 }
